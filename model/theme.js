5e0badcb8457c268a751224f0c88efb96e8226a6
anytain <1732846832@qq.com>
2023-08-12 21:23:10 +0800

    0812提交

--- model/theme.js
+++ model/theme.js
@@ -1,12 +1,37 @@
 import {Http} from "../utils/http";
 
 class Theme {
-    static async getHomeLocationA() {
-        return await Http.request({
+    static locationA = 't-1';
+    static locationE = 't-2';
+    static locationF = 't-3';
+    static locationH = 't-4';
+    themes = []
+
+    async getThemes() {
+        const names = `${Theme.locationA},${Theme.locationE},${Theme.locationF},${Theme.locationH}`
+        this.themes = await Http.request({
             url: `theme/by/names`,
             data: {
-                names: 't-1'
+                names: names
             }
+        })
+    }
+
+    async getHomeLocationA() {
+        return this.themes.find(t => t.name === Theme.locationA)
+    }
+
+    async getHomeLocationE() {
+        return this.themes.find(t => t.name === Theme.locationE)
+    }
+
+    static  getHomeLocationnESpu() {
+        return Theme.getThemeSpuByName(Theme.locationE)
+    }
+
+    static  getThemeSpuByName(name) {
+        return  Http.request({
+            url: `theme/name/${name}/with_spu`
         })
     }
 }
--- pages/home/home.js
+++ pages/home/home.js
@@ -3,81 +3,99 @@
 import {config} from "../../config/config";
 import {Theme} from "../../model/theme";
 import {Banner} from "../../model/banner";
+import {Category} from "../../model/category";
+import {Activity} from "../../model/activity";
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    themeA:null,
-    bannerB:null
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        themeA: null,
+        bannerB: null,
+        grid: [],
+        activityD: null,
+        themeE: null
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    async onLoad(options) {
+        this.initAllData()
+    },
+    async initAllData() {
+        const theme = new Theme()
+        await theme.getThemes()
+        const themeA = await theme.getHomeLocationA();
+        const themeE = await theme.getHomeLocationE();
+        let themeESpu = []
+        if(themeE.online){
+            const data = await Theme.getHomeLocationnESpu()
+            if(data){
+                themeESpu = data.spu_list.slice(0,8)
+            }
+        }
+        const bannerB = await Banner.getHomeLocationB()
+        const grid = await Category.getGridCategory()
+        const activityD = await Activity.getHomeLocationD()
+        this.setData({
+            themeA,
+            bannerB,
+            grid,
+            activityD,
+            themeE,
+            themeESpu
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  async onLoad(options) {
-   this.initAllData()
-  },
-  async initAllData() {
-    const themeA = await Theme.getHomeLocationA()
-    const bannerB = await Banner.getHomeLocationB()
-    console.log("1111");
-    console.log(bannerB);
-    this.setData({
-      themeA:themeA[0],
-      bannerB:bannerB
-    })
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    },
 
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+    }
 })
